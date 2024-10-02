c22a454d9412f88b64239e05f8c8a6d405cbd55f
Valeria <timohinava2005@gmail.com>
2024-10-02 13:09:00 +0700

    Все задания соединены в один файл

--- main.cpp
+++ main.cpp
@@ -437,6 +437,70 @@ void testIterators() {
 }
 
 int main() {
-    testIterators(); // Запуск функции-теста
+    std::cout << "Пользовательский код с демонстрацией возможностей последовательного контейнера." << std::endl;
+    Array<int> array;
+    for (int i = 0; i < 10; i++) {
+        array.push_back(i);
+    }
+    std::cout << "В контейнер добавлены числа от 0 до 9: ";
+    array.print();
+    std::cout << std::endl;
+    std::cout << "Размер контейнера: " << array.get_size() << std::endl;
+    array.erase(2);
+    array.erase(3);
+    array.erase(4);
+    std::cout << "Из контейнера удалили 3, 5 и 7 элементы (по счету): ";
+    array.print();
+    std::cout << std::endl;
+    array.insert(0, 10);
+    std::cout << "В начало контейнера добавили 10: ";
+    array.print();
+    std::cout << std::endl;
+    array.insert(array.get_size() / 2, 20);
+    std::cout << "В середину контейнера добавили 20: ";
+    array.print();
+    std::cout << std::endl;
+    array.push_back(30);
+    std::cout << "В конец контейнера добавили 30: ";
+    array.print();
+    std::cout << std::endl;
+    std::cout << "Пользовательский код с демонстрацией возможностей контейнера спискового типа." << std::endl;
+    BidirectionalList<int> list;
+    for (int i = 0; i < 10; i++) {
+        list.push_back(i);
+    }
+    std::cout << "В контейнер добавлены числа от 0 до 9: ";
+    list.print();
+    std::cout << std::endl;
+    std::cout << "Размер контейнера: " << list.get_numberOfElements() << std::endl;
+    list.erase(2);
+    list.erase(3);
+    list.erase(4);
+    std::cout << "Из контейнера удалили 3, 5 и 7 элементы (по счету): ";
+    list.print();
+    std::cout << std::endl;
+    list.insert(0, 10);
+    std::cout << "В начало контейнера добавили 10: ";
+    list.print();
+    std::cout << std::endl;
+    list.insert(list.get_numberOfElements() / 2, 20);
+    std::cout << "В середину контейнера добавили 20: ";
+    list.print();
+    std::cout << std::endl;
+    list.push_back(30);
+    std::cout << "В конец контейнера добавили 30: ";
+    list.print();
+    std::cout << std::endl;
+    std::cout << "Пользовательский код с демонстрацией возможностей последовательного контейнера с резервированием памяти." << std::endl;
+    Array<int> array2;
+    for (int i = 0; i < 10; i++) {
+        array2.push_back(i);
+    }
+    std::cout << "В контейнер добавлены числа от 0 до 9: ";
+    array2.print();
+    std::cout << std::endl;
+    std::cout << "Количество элементов в контейнере: " << array2.get_size() << std::endl;
+    std::cout << "Размер контейнера: " << array.get_selectedSize() << std::endl;
+    testIterators();
     return 0;
 }
